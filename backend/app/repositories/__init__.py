ebf3f5809f0103b3fce5fbdc2030879bbdd66415
moha100h <mmlu.tel@gmail.com>
2026-06-01 16:17:31 +0330

    feat: Repositories __init__

---
+++ backend/app/repositories/__init__.py
@@ -0,0 +1,5 @@
+from app.repositories.base import BaseRepository
+from app.repositories.user_repo import UserRepository
+from app.repositories.order_repo import OrderRepository
+
+__all__ = ["BaseRepository", "UserRepository", "OrderRepository"]
